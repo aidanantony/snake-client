df2d4d240152c19aba14bc847e9c8e20471d2d66
Aidan Antony <aidanantony65@gmail.com>
2022-04-21 15:59:29 -0600

    Modifying my file to now move up on connect

--- client.js
+++ client.js
@@ -6,15 +6,18 @@ const connect = function () {
   });
   conn.on('connect', (connect) => {
     console.log('Succesfully connnected to game server')
+    conn.write('Name: AJA');
   });
-
+  conn.on("connect", () => {
+    conn.write("Move: up")
+  });
+  
   conn.on("data", (data) => {
     console.log(data)
   });
 
-  conn.on('connect', () => {
-    conn.write('Name: AJA');
-  });
+  
+  
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
