20283d68d4f64fa0ba1d8ccfa5b63ccc07c491ef
Aidan Antony <aidanantony65@gmail.com>
2022-04-22 12:03:05 -0600

    My updated files for my snek project

--- client.js
+++ client.js
@@ -1,18 +1,19 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.on('connect', () => {
     console.log('Succesfully connnected to game server');
     conn.write('Name: AJA');
   });
 
-  // conn.on("data", (data) => {
-  //   console.log(data)
+  conn.on("data", (data) => {
+    console.log(data)
   //   conn.write("Say: You can't catch me")
-  // });
+  });
   
 
   
